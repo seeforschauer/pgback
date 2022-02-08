decf6330ee61f14e4807661fd944fe59f441d8c1
Ekaterina Blech <blech.kate@ya.ru>
2022-02-08 08:28:20 +0300

    some code

--- internal/base.go
+++ internal/base.go
@@ -36,6 +36,50 @@ func connect(dbConfig config.DB) error {
 	return nil
 }
 
+type TableName struct {
+	DatName string
+	Oid     string
+}
+
+// \d \d+
+func getTableNames() ([]TableName, error) {
+	q := "SELECT datname, oid from pg_database"
+	rows, err := conn.Query(q)
+	if err != nil {
+		return nil, fmt.Errorf("conn Query: %w", err)
+	}
+	defer rows.Close()
+
+	var tableNames []TableNames
+	for rows.Next() {
+		dt := ableNames
+		if err := rows.Scan(&dt); err != nil {
+			return nil, fmt.Errorf("rows Scan: %w", err)
+		}
+		tableNames = append(tableNames, dt)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("conn rows err: %w", err)
+	}
+	return tableNames, nil
+}
+
+//backupTables dd alive
+func backupTables(tables []TableName) error
+	// COPY TO
+
+	for i := range tables {
+		table := tables[i]
+		fname:= table.DatName + ".dat"
+		f, err := os.Create(fname)
+		if err != nil {
+			return fmt.Errorf("osCreate %w", err)
+		}
+		defer f.Close()
+		q := fmt.Sprintf("COPY %s TO stdout", table.DatName)
+	}
+}
+
 // for tables, select datname, oid from pg_database
 // per table: SELECT * FROM pg_catalog.pg_tables
 // filepath.Join  PGDATA base / oid
